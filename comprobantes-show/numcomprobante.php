380e4cac8e5060a4b37e92e686d64b5a53821f3c
Andres <polachgg@gmail.com>
2017-12-13 11:41:01 -0300

    asdfghj

--- comprobantes-show/numcomprobante.php
+++ comprobantes-show/numcomprobante.php
@@ -1,5 +1,5 @@
 <?php 
-$compSql ="SELECT nro_det_comp_entidad_liq_prest, tipo_comp, letra_comp, nro_pto_vta_comp, nro_comp FROM det_comp_entidad_liq_prest WHERE nro_lote_entidad_liq_prest=".;
-$compQuery = pg_query($con, );
+$compSql ="SELECT nro_det_comp_entidad_liq_prest, tipo_comp, letra_comp, nro_pto_vta_comp, nro_comp FROM det_comp_entidad_liq_prest WHERE nro_lote_entidad_liq_prest=";
+$compQuery = pg_query($con, $compSql);
 
 ?>
